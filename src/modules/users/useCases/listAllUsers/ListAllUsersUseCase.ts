c3168200a032e27e81012b78cb967af8c0c68f2e
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-11-22 22:52:52 -0400

    Desafio concluído

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -9,7 +10,17 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User doesn't exists");
+    }
+
+    if (!user.admin) {
+      throw new Error("User isn't a admin");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Router } from "express";
 
 import { createUserController } from "../modules/users/useCases/createUser";
@@ -7,20 +8,20 @@ import { turnUserAdminController } from "../modules/users/useCases/turnUserAdmin
 
 const usersRoutes = Router();
 
-usersRoutes.post("/", (request, response) =>
-  createUserController.handle(request, response)
-);
+usersRoutes.post("/", (request, response) => {
+  return createUserController.handle(request, response);
+});
 
-usersRoutes.patch("/:user_id/admin", (request, response) =>
-  turnUserAdminController.handle(request, response)
-);
+usersRoutes.patch("/:user_id/admin", (request, response) => {
+  return turnUserAdminController.handle(request, response);
+});
 
-usersRoutes.get("/:user_id", (request, response) =>
-  showUserProfileController.handle(request, response)
-);
+usersRoutes.get("/:user_id", (request, response) => {
+  return showUserProfileController.handle(request, response);
+});
 
-usersRoutes.get("/", (request, response) =>
-  listAllUsersController.handle(request, response)
-);
+usersRoutes.get("/", (request, response) => {
+  return listAllUsersController.handle(request, response);
+});
 
 export { usersRoutes };
